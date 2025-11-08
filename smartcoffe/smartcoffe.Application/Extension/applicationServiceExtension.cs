5b63de55b6b1edc21fc7827e18bc7a1a689c73d1
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-11-08 08:20:46 -0500

    refator/handlers promotion implemented

--- smartcoffe/smartcoffe.Application/Extension/applicationServiceExtension.cs
+++ smartcoffe/smartcoffe.Application/Extension/applicationServiceExtension.cs
@@ -1,5 +1,10 @@
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using smartcoffe.Application.Promotion.Commands.CreatePromotion;
+using smartcoffe.Application.Promotion.Commands.DeletePromotion;
+using smartcoffe.Application.Promotion.Commands.UpdatePromotion;
+using smartcoffe.Application.Promotion.Queries.PromotionGet;
+using smartcoffe.Application.Promotion.Queries.PromotionGetById;
 
 namespace smartcoffe.Application.Extension;
 
@@ -12,6 +17,11 @@ public class applicationServiceExtension
             services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.RegisterServicesFromAssembly(typeof(createPromotionHandler).Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(updatePromotionHandler).Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(deletePromotionHandler).Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(getAllPromotionsHandler).Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(getPromotionByIdHandler).Assembly);
             });
         }
     }
