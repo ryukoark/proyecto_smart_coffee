55719e722435ee41d76e5b5220bba25cfb59d131
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2025-11-08 10:15:53 -0500

    Add ShoppingController and delete shopping feature
    Introduced ShoppingController with CRUD endpoints for shopping entities. Added DeleteShoppingCommand and handler to support deletion. Updated Program.cs to register API explorer, enable authorization, and map controllers.

---
+++ smartcoffe/smartcoffe.Application/Features/Shopping/Commands/DeleteShoppingCommand.cs
@@ -0,0 +1,35 @@
+using MediatR;
+
+namespace smartcoffe.Application.Features.Shopping.Commands
+{
+    public class DeleteShoppingCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+
+        public DeleteShoppingCommand(int id)
+        {
+            Id = id;
+        }
+    }
+
+    public class DeleteShoppingCommandHandler : IRequestHandler<DeleteShoppingCommand, bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteShoppingCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(DeleteShoppingCommand request, CancellationToken cancellationToken)
+        {
+            var shopping = await _unitOfWork.Products.GetByIdAsync(request.Id);
+            if (shopping == null) return false;
+
+            _unitOfWork.Products.Remove(shopping);
+            await _unitOfWork.CompleteAsync();
+
+            return true;
+        }
+    }
+}
--- smartcoffe/smartcoffe/Program.cs
+++ smartcoffe/smartcoffe/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
@@ -13,6 +14,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.Mapcontrollers();
 
 var summaries = new[]
 {
