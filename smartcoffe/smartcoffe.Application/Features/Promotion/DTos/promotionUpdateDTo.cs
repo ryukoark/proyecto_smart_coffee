92a8456b503b9e6d10cea37ec8aaab2f394135d8
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-11-08 01:03:13 -0500

    feat/promotionController implemented

--- smartcoffe/smartcoffe.Application/Features/Promotion/DTos/promotionUpdateDTo.cs
+++ smartcoffe/smartcoffe.Application/Features/Promotion/DTos/promotionUpdateDTo.cs
@@ -2,6 +2,7 @@ namespace smartcoffe.Application.Promotion.DTos;
 
 public class promotionUpdateDTo
 {
+    public int id { get; set; }
     public string name { get; set; }
     public decimal amount { get; set; }
     public string type { get; set; }
