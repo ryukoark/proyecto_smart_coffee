a510896c0ce55d4aaf884bd581ff9cd477bd56b4
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-12-03 02:36:13 -0500

    services implemented

--- smartcoffe/smartcoffe.Application/Features/modulo_compras/ShoppingDetail/Commands/CreateShoppingDetailCommand.cs
+++ smartcoffe/smartcoffe.Application/Features/modulo_compras/ShoppingDetail/Commands/CreateShoppingDetailCommand.cs
@@ -17,14 +17,28 @@ namespace smartcoffe.Application.Features.modulo_compras.ShoppingDetail.Commands
     public class CreateShoppingDetailCommandHandler : IRequestHandler<CreateShoppingDetailCommand, int>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IInventoryService _inventoryService;
 
-        public CreateShoppingDetailCommandHandler(IUnitOfWork unitOfWork)
+        public CreateShoppingDetailCommandHandler(IUnitOfWork unitOfWork, IInventoryService inventoryService)
         {
             _unitOfWork = unitOfWork;
+            _inventoryService = inventoryService;   
         }
 
         public async Task<int> Handle(CreateShoppingDetailCommand request, CancellationToken cancellationToken)
         {
+            
+            var dto = request.ShoppingDetail;
+            
+            // Verificar stock
+            var hasStock = await _inventoryService.CheckStockAsync(dto.Id, dto.Quantity);
+
+            if (!hasStock)
+                throw new Exception("Insufficient stock for this product.");
+
+            // Descontar stock
+            await _inventoryService.ReduceStockAsync(dto.Id, dto.Quantity);
+            
             var shoppingDetail = new Domain.Entities.ShoppingDetail
             {
                 IdProduct = null, // Ajusta según los datos del DTO
--- smartcoffe/smartcoffe.Application/Features/modulo_compras/ShoppingDetail/DTOs/ShoppingDetailCreateDto.cs
+++ smartcoffe/smartcoffe.Application/Features/modulo_compras/ShoppingDetail/DTOs/ShoppingDetailCreateDto.cs
@@ -2,8 +2,10 @@ namespace smartcoffe.Application.Features.modulo_compras.ShoppingDetail.DTOs
 {
     public class ShoppingDetailCreateDto
     {
+        public int Id {get; set;}
         public string ProductName { get; set; }
         public decimal Price { get; set; }
+        public int Quantity { get; set; }
         public string BuyerName { get; set; }
         public string PaymentMethod { get; set; }
     }
